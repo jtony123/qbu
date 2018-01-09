f6e6bf34cf50e1c330a7de4d0f1ef9cea87023f5
Anthony J <anthony.jackson@orreco.com>
2018-01-09 09:11:23 +0000

    pressRelease added

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package controllers;
 
+
+import java.io.File;
+
 import play.mvc.*;
 
 import views.html.*;
@@ -17,7 +20,32 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
+    	if(request().remoteAddress() != null){
+        	System.out.println("index page accessed from " + request().remoteAddress());
+    	}
         return ok(index.render());
     }
+    
+    
+    public Result pressRelease() {
+    	
+    	return ok(pressrelease.render());
+    }
+    
+    
+	public Result downloadFile() {
+		String filepath = "/tmp/QBUPressRelease.pdf";
+		
+		File file = new java.io.File(filepath);
+		if(file.exists()){
+			System.out.println("file found");
+			return ok(new java.io.File(filepath));
+		} else {
+			System.out.println("file not found");
+			return ok(new java.io.File("data/QBUPressRelease.pdf"));
+		}
+		
+		//return ok(new java.io.File(filepath));
+	}
 
 }
--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := """qbu"""
 
-version := "1.0.5"
+version := "1.0.10"
 
 lazy val root = (project in file(".")).enablePlugins(PlayJava)
 
@@ -12,4 +12,7 @@ libraryDependencies ++= Seq(
   javaWs,
   "com.typesafe.play" %% "play-mailer" % "5.0.0-M1"
 )
+
+import com.typesafe.sbt.packager.MappingsHelper._
+    mappings in Universal ++= directory(baseDirectory.value / "data")
 
